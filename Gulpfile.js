5b0160f7920a37004da7ea82eadf420a8138a9c3
Jamie Barton <hi_jamie@me.com>
2017-01-23 14:35:21 +0000

    Added revision and imagemin to Gulp process

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,9 +3,14 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
+const imagemin = require('gulp-imagemin');
+const revDel = require('gulp-rev-delete-original');
+const pngquant = require('imagemin-pngquant');
+const rev = require('gulp-rev');
 const nodemon = require('gulp-nodemon');
 const browserSync = require('browser-sync');
 const reload = browserSync.reload;
+const rimraf = require('rimraf');
 
 const APP_PORT = 5000;
 
@@ -14,12 +19,22 @@ gulp.task('sass', () => {
     .pipe(sass({
       outputStyle: 'compressed'
     }).on('error', sass.logError))
-    .pipe(autoprefixer("last 2 versions", "> 1%", "ie 8"))
+    .pipe(autoprefixer("last 2 versions", "> 1%"))
     .pipe(gulp.dest('./public/assets'));
+});
+
+gulp.task('imagemin', () => {
+  return gulp.src('./assets/images/**')
+    .pipe(imagemin({
+      progressive: true,
+      use: [pngquant()]
+    }))
+    .pipe(gulp.dest('./public/assets/images/'))
 });
 
 gulp.task('watch', () => {
   gulp.watch('./assets/scss/**/*.scss', ['sass']);
+  gulp.watch('./assets/assets/images/**', ['imagemin']);
 });
 
 gulp.task('browser-sync', () => {
@@ -51,6 +66,30 @@ gulp.task('nodemon', () => {
     this.stderr.pipe(process.stderr);
   });
 });
+
+gulp.task('clean', (cb) => {
+  rimraf('./public/assets', cb);
+});
+
+gulp.task('revision', () => {
+  return gulp.src([
+      './public/assets/*.css',
+      './public/assets/*.js'
+    ], {
+      base: './public/assets/'
+    })
+    .pipe(gulp.dest('./public/assets'))
+    .pipe(rev())
+    .pipe(revDel())
+    .pipe(gulp.dest('./public/assets'))
+    .pipe(rev.manifest({
+      path: 'manifest.json',
+      merge: true
+    }))
+    .pipe(gulp.dest('./public/assets'));
+});
+
+gulp.task('build', ['clean', 'sass', 'imagemin', 'revision'])
 
 gulp.task('default', [
   'sass',
